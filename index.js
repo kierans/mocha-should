d398ecb365732ab66444d3ae38161fa5afaf69fb
Kieran Simpson <kierans777@gmail.com>
2017-05-14 22:18:23 +1000

    Updated module to work in both browser and node.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 "use strict";
 
-module.exports = function(message, test) {
-  it("should " + message, test);
-};
+(function(window) {
+  function should(message, test) {
+    it("should " + message, test);
+  }
+
+  if (typeof module !== "undefined") {
+    module.exports = should;
+
+    // might as well do this since Mocha defines `global.it`
+    global.should = should;
+  }
+  else {
+    window.should = should;
+  }
+})(window);
